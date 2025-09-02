b38cb88c0edbfb67e10cf883b052e6e3e4a1a5ae
sniffy <manibabu@live.com>
2025-09-02 11:59:00 +0530

    Adding the docker based env

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -54,7 +54,7 @@ const loadConfigOptions = {
     useAwsSecrets: true,
     useLocalEnvOverride: process.env.USE_LOCAL_ENV_OVERRIDE === 'true',
     loadDotenv: true,
-    dotenvPath: '../deploy/.env',
+    dotenvPath: '../docker/.env',
 };
 
 // Create async config loader function
